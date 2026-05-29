7e16f97dbdf5288cf53d923476068a2c84e2209b
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-29 16:57:20 +0530

    refactor: update RootLayout component and remove unused Index screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,11 @@
+// external-imports
 import { Stack } from 'expo-router';
 
+// function to render the root layout
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
