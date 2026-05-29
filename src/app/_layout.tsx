d3a915c81275fe75dfe6042750340e8d2ab939b5
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-29 22:14:51 +0530

    feat: initialize database and create snippets table

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,17 @@
+// internal-imports
+import { initDB } from '@/db/index';
+
 // external-imports
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
 
 // function to render the root layout
 export default function RootLayout() {
+  // useEffect to perform startup tasks
+  useEffect(() => {
+    initDB();
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+// external-imports
+import * as SQLite from 'expo-sqlite';
+
+// open the database connection
+export const db = SQLite.openDatabaseSync('devsnippets.db');
+
+export function initDB() {
+  // create the snippets table if it doesn't exist
+  db.execAsync(`CREATE TABLE IF NOT EXISTS snippets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    code TEXT NOT NULL,
+    language TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )`);
+}
