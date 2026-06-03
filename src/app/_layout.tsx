434dba5f990d28a3d69f93cf1be108fae959186f
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-04 01:42:26 +0530

    feat: restructure RootLayout to include global.css, StatusBar, GestureHandler and HeroUI providers

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,13 @@
 // internal-imports
+import '@/global.css';
 import { initDB } from '@/db/index';
 
 // external-imports
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { HeroUINativeProvider } from 'heroui-native/provider';
 import { useEffect } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 // function to render the root layout
 export default function RootLayout() {
@@ -13,8 +17,13 @@ export default function RootLayout() {
   }, []);
 
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+        <StatusBar style="auto" animated />
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
   );
 }
