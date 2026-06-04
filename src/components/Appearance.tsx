94cd81aa8328e8e814d1ac872d621c03603713be
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-05 01:42:38 +0530

    feat: add gap styling to Appearance component for improved layout

--- src/components/Appearance.tsx
+++ src/components/Appearance.tsx
@@ -24,7 +24,7 @@ export default function Appearance() {
   }
 
   return (
-    <View>
+    <View className="gap-y-4">
       <Typography.Heading type="h6">Appearance</Typography.Heading>
       <ListGroup className="w-full">
         <BottomSheet isOpen={isOpen} onOpenChange={setIsOpen}>
