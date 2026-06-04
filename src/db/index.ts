fa8bbaead54f3bc4023cadd29203a68d759b1e2a
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-05 03:40:14 +0530

    fix: change database connection to async and await in initDB function

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,11 +2,14 @@
 import * as SQLite from 'expo-sqlite';
 
 // open the database connection
-export const db = SQLite.openDatabaseSync('devsnippets.db');
+export const database = SQLite.openDatabaseAsync('devsnippets.db');
 
-export function initDB() {
+export async function initDB() {
+  // get the database connection
+  const db = await database;
+
   // create the snippets table if it doesn't exist
-  db.execAsync(`CREATE TABLE IF NOT EXISTS snippets (
+  await db.execAsync(`CREATE TABLE IF NOT EXISTS snippets (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     code TEXT NOT NULL,
