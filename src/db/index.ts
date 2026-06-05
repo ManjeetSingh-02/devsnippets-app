f4138ce622b1b93b963922d39f7cd364f257a936
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-05 21:26:20 +0530

    feat: add default value for favourite column in snippets table

--- src/db/index.ts
+++ src/db/index.ts
@@ -15,6 +15,7 @@ export async function initDB() {
     code TEXT NOT NULL,
     language TEXT NOT NULL,
     tags TEXT,
+    favourite INTEGER NOT NULL DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
   )`);
 }
