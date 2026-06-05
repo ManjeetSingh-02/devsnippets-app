f48e10857410e42603d3c60ed15bfdc46bdd7fde
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-05 21:24:39 +0530

    feat: improve formatBytes function to correctly handle byte conversion to MB and GB

--- src/utils/format-bytes.ts
+++ src/utils/format-bytes.ts
@@ -3,5 +3,7 @@ export function formatBytes(bytes: number) {
 
   if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(2)} KB`;
 
-  return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+  if (bytes < 1024 * 1024 * 1024) return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+
+  return `${(bytes / (1024 * 1024 * 1024)).toFixed(2)} GB`;
 }
