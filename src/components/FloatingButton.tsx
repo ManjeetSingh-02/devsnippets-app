bcdb861dc28a93de7a75cbfed500e5022f0d06a1
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-06 00:56:43 +0530

    feat: enhance FloatingButton to navigate to snippet creation page

--- src/components/FloatingButton.tsx
+++ src/components/FloatingButton.tsx
@@ -1,4 +1,5 @@
 // external-imports
+import { useRouter } from 'expo-router';
 import { Button } from 'heroui-native';
 import { Plus } from 'lucide-react-native';
 import { useUniwind } from 'uniwind';
@@ -8,8 +9,23 @@ export default function FloatingButton() {
   const { theme } = useUniwind();
   const iconColor = theme === 'dark' ? 'white' : 'black';
 
+  // get the router instance from expo-router
+  const router = useRouter();
+
+  // function to handle button press
+  function handlePress() {
+    // navigate to the create page
+    router.push('/snippet/create');
+  }
+
   return (
-    <Button isIconOnly variant="outline" className="absolute bottom-4 right-4" size="lg">
+    <Button
+      isIconOnly
+      variant="outline"
+      className="absolute bottom-4 right-4"
+      size="lg"
+      onPress={handlePress}
+    >
       <Plus size={24} color={iconColor} />
     </Button>
   );
