e5a2cf99357d9b504d9417e6bf863c07f6ca7ebd
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-05 17:26:09 +0530

    feat: update initDB call in RootLayout to use void for better promise handling

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -13,7 +13,7 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 export default function RootLayout() {
   // useEffect to perform startup tasks
   useEffect(() => {
-    initDB();
+    void initDB();
   }, []);
 
   return (
