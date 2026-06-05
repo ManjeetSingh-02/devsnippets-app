7c9885205010a9a50c1f5357ba935411131c1d72
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-06 00:43:58 +0530

    feat: refactor Home component to use ScrollView for better layout and add snippet heading

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,17 +1,25 @@
 // internal-imports
+import FloatingButton from '@/components/FloatingButton';
+import Header from '@/components/Header';
 import SafeScreen from '@/components/SafeScreen';
 
 // external-imports
 import { Typography } from 'heroui-native';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 
 // function to render the home screen
 export default function Home() {
   return (
     <SafeScreen>
-      <View className="flex-1 items-center justify-center bg-white dark:bg-black">
-        <Typography>Home</Typography>
-      </View>
+      <ScrollView
+        className="flex-1"
+        contentContainerClassName="px-4 py-6 gap-6"
+        showsVerticalScrollIndicator={false}
+      >
+        <Typography.Heading type="h1">Your Snippets</Typography.Heading>
+        <Header />
+      </ScrollView>
+      <FloatingButton />
     </SafeScreen>
   );
 }
