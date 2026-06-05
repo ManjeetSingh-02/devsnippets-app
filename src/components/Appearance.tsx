cdeae5c2835da5b6d1ecec104b9ee3ca00ac4b83
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-05 15:01:06 +0530

    feat: enhance Appearance component with animated chevron and dynamic theme display

--- src/components/Appearance.tsx
+++ src/components/Appearance.tsx
@@ -1,8 +1,9 @@
 // external-imports
-import { BottomSheet, ListGroup, RadioGroup, Typography } from 'heroui-native';
-import { ChevronDown, ChevronRight, SunMoon } from 'lucide-react-native';
+import { BottomSheet, ListGroup, PressableFeedback, RadioGroup, Typography } from 'heroui-native';
+import { ChevronRight, SunMoon } from 'lucide-react-native';
 import { useState } from 'react';
 import { View } from 'react-native';
+import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated';
 import { Uniwind, useUniwind } from 'uniwind';
 
 // type for theme
@@ -14,7 +15,10 @@ export default function Appearance() {
   const [isOpen, setIsOpen] = useState(false);
 
   // get the current theme from uniwind
-  const { theme } = useUniwind();
+  const { theme, hasAdaptiveThemes } = useUniwind();
+  const selectedTheme = hasAdaptiveThemes ? 'system' : theme;
+
+  // set the icon color based on the current theme
   const iconColor = theme === 'dark' ? 'white' : 'black';
 
   // function to handle theme change
@@ -23,33 +27,48 @@ export default function Appearance() {
     setIsOpen(false);
   }
 
+  // function to animate the chevron icon when the bottom sheet is opened or closed
+  const chevronAnimation = useAnimatedStyle(() => ({
+    transform: [
+      {
+        rotate: withTiming(isOpen ? '90deg' : '0deg', { duration: 200 }),
+      },
+    ],
+  }));
+
   return (
     <View className="gap-y-2">
       <Typography.Heading type="h6">Appearance</Typography.Heading>
       <ListGroup className="w-full">
         <BottomSheet isOpen={isOpen} onOpenChange={setIsOpen}>
           <BottomSheet.Trigger asChild>
-            <ListGroup.Item>
-              <ListGroup.ItemPrefix>
-                <SunMoon size={24} color={iconColor} />
-              </ListGroup.ItemPrefix>
-              <ListGroup.ItemContent>
-                <ListGroup.ItemTitle>Theme</ListGroup.ItemTitle>
-                <ListGroup.ItemDescription>System, Light or Dark</ListGroup.ItemDescription>
-              </ListGroup.ItemContent>
-              <ListGroup.ItemSuffix>
-                {isOpen ? (
-                  <ChevronDown size={24} color={iconColor} />
-                ) : (
-                  <ChevronRight size={24} color={iconColor} />
-                )}
-              </ListGroup.ItemSuffix>
-            </ListGroup.Item>
+            <PressableFeedback asChild>
+              <ListGroup.Item>
+                <ListGroup.ItemPrefix>
+                  <SunMoon size={24} color={iconColor} />
+                </ListGroup.ItemPrefix>
+                <ListGroup.ItemContent>
+                  <ListGroup.ItemTitle>Theme</ListGroup.ItemTitle>
+                  <ListGroup.ItemDescription>
+                    {selectedTheme.charAt(0).toUpperCase() + selectedTheme.slice(1)}
+                  </ListGroup.ItemDescription>
+                </ListGroup.ItemContent>
+                <ListGroup.ItemSuffix>
+                  <Animated.View style={chevronAnimation}>
+                    <ChevronRight size={24} color={iconColor} />
+                  </Animated.View>
+                </ListGroup.ItemSuffix>
+              </ListGroup.Item>
+            </PressableFeedback>
           </BottomSheet.Trigger>
           <BottomSheet.Portal>
             <BottomSheet.Overlay />
             <BottomSheet.Content>
-              <RadioGroup value={theme} onValueChange={handleThemeChange}>
+              <RadioGroup
+                variant="secondary"
+                value={selectedTheme}
+                onValueChange={handleThemeChange}
+              >
                 <RadioGroup.Item value="system">System</RadioGroup.Item>
                 <RadioGroup.Item value="light">Light</RadioGroup.Item>
                 <RadioGroup.Item value="dark">Dark</RadioGroup.Item>
