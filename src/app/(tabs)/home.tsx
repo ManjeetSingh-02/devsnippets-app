0fca689aa674499d0edce90dc1828c8c3488dc74
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-07 04:11:00 +0530

    refactor: replace ScrollView with nested View for improved layout structure

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -2,23 +2,23 @@
 import FloatingButton from '@/components/FloatingButton';
 import Header from '@/components/Header';
 import SafeScreen from '@/components/SafeScreen';
+import SnippetList from '@/components/SnippetList';
 
 // external-imports
 import { Typography } from 'heroui-native';
-import { ScrollView } from 'react-native';
+import { View } from 'react-native';
 
 // function to render the home screen
 export default function Home() {
   return (
     <SafeScreen>
-      <ScrollView
-        className="flex-1"
-        contentContainerClassName="px-4 py-6 gap-6"
-        showsVerticalScrollIndicator={false}
-      >
-        <Typography.Heading type="h1">Your Snippets</Typography.Heading>
-        <Header />
-      </ScrollView>
+      <View className="flex-1">
+        <View className="px-4 py-6 gap-6">
+          <Typography.Heading type="h1">Your Snippets</Typography.Heading>
+          <Header />
+        </View>
+        <SnippetList />
+      </View>
       <FloatingButton />
     </SafeScreen>
   );
