6faf09ce67c0795bd7e279ef87706c6ba0831564
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-07 04:10:50 +0530

    feat: implement SnippetList component to display snippets with loading and error handling

---
+++ src/components/SnippetList.tsx
@@ -0,0 +1,102 @@
+// internal-imports
+import { getAllSnippets } from '@/db';
+import { SnippetPreview } from '@/types/snippet';
+
+// external-imports
+import { Card, Chip, Spinner, Typography } from 'heroui-native';
+import { Heart } from 'lucide-react-native';
+import { useState, useEffect } from 'react';
+import { FlatList, View } from 'react-native';
+import { useUniwind } from 'uniwind';
+
+// function component for the snippet list
+export default function SnippetList() {
+  // get the current theme from uniwind
+  const { theme } = useUniwind();
+  const iconColor = theme === 'dark' ? 'white' : 'black';
+
+  // state for the data and UI
+  const [snippetsData, setSnippetsData] = useState<SnippetPreview[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  // function to render each item in the snippet list
+  function renderItemComponent({ item }: { item: SnippetPreview }) {
+    return (
+      <Card className="mx-3 mb-3">
+        <Card.Header>
+          <Chip size="md" variant="tertiary" color="default">
+            {item.language.toUpperCase()}
+          </Chip>
+        </Card.Header>
+        <Card.Body className="gap-y-4 p-4">
+          <View className="flex-row items-start justify-between">
+            <Card.Title>{item.title}</Card.Title>
+            <Heart size={20} color={iconColor} fill={item.favourite ? iconColor : 'transparent'} />
+          </View>
+        </Card.Body>
+        <Card.Footer className="flex-row items-center gap-2">
+          {item.tags
+            ?.split(',')
+            .map(tag => tag.trim())
+            .filter(Boolean)
+            .map((tag, i) => (
+              <Chip variant="secondary" color="default" key={i} size="sm">
+                {tag}
+              </Chip>
+            ))}
+        </Card.Footer>
+      </Card>
+    );
+  }
+
+  // function to render the empty component when there are no snippets
+  function renderEmptyComponent() {
+    return (
+      <Typography type="h4" className="text-center mt-10">
+        Create your first snippet from the + button.
+      </Typography>
+    );
+  }
+
+  // useEffect to fetch the snippets when the component mounts
+  useEffect(() => {
+    // function to fetch all snippets from the database
+    async function fetchSnippet() {
+      try {
+        // fetch the snippets data from the database
+        const data = await getAllSnippets();
+
+        // set the snippets data in state
+        setSnippetsData(data);
+      } catch (error) {
+        // log the error
+        console.error(error);
+
+        // set error state to true
+        setError(true);
+      } finally {
+        // set loading state to false
+        setIsLoading(false);
+      }
+    }
+
+    // call the fetchSnippet function
+    fetchSnippet();
+  }, []);
+
+  return isLoading ? (
+    <Spinner size="lg" color={iconColor} className="mt-10 self-center" />
+  ) : error ? (
+    <Typography type="h4" className="text-center mt-10">
+      Failed to load snippets.
+    </Typography>
+  ) : (
+    <FlatList
+      data={snippetsData}
+      keyExtractor={item => String(item.id)}
+      renderItem={renderItemComponent}
+      ListEmptyComponent={renderEmptyComponent}
+    />
+  );
+}
